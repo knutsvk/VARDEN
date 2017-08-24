35cbd532c5a48f779d02db2cdf81b66353eb2126
knutsvk <ksk38@cam.ac.uk>
2017-08-24 11:51:15 +0100

    removed bingham viscosity from viscosity update function

--- src/viscosity.f90
+++ src/viscosity.f90
@@ -13,7 +13,7 @@ module viscosity_module
 
 contains
 
-  subroutine update_viscosity(mla, viscosity, strain_rate, dx, the_bc_level)
+  subroutine update_viscosity(mla, viscosity, phi, dx, the_bc_level)
 
     use bl_constants_module
     use ml_restrict_fill_module
@@ -21,7 +21,7 @@ contains
 
     type(ml_layout)   , intent(in   ) :: mla
     type(multifab)    , intent(inout) :: viscosity(:)
-    type(multifab)    , intent(in   ) :: strain_rate(:)
+    type(multifab)    , intent(in   ) :: phi(:)
     real(kind = dp_t) , intent(in   ) :: dx(:,:)
     type(bc_level)    , intent(in   ) :: the_bc_level(:)
 
@@ -44,12 +44,12 @@ contains
 
        do i = 1, nfabs(viscosity(n))
           vp => dataptr(  viscosity(n),i)
-          sp => dataptr(strain_rate(n),i)
+          pp => dataptr(        phi(n),i)
           lo = lwb(get_box(viscosity(n),i))
           hi = upb(get_box(viscosity(n),i))
           select case (dm)
           case (2)
-             call update_viscosity_2d(vp(:,:,1,1), sp(:,:,1,1), lo, hi, ng, dx(n,:))
+             call update_viscosity_2d(vp(:,:,1,1), pp(:,:,1,1), lo, hi, ng, dx(n,:))
           case (3)
           !   call update_viscosity_3d(viscp(:,:,:,:), dgp(:,:,:,:), lo, hi, ng_visc, ng_dg, dx(n,:))
           end select
@@ -65,14 +65,14 @@ contains
 
   end subroutine update_viscosity
 
-  subroutine update_viscosity_2d(viscosity, strain_rate, lo, hi, ng, dx)
+  subroutine update_viscosity_2d(viscosity, phi, lo, hi, ng, dx)
 
     use bl_constants_module
-    use probin_module, only: visc_coef, yield_stress, papa_reg
+    use probin_module, only: visc_coef
 
     integer           , intent(in   ) :: lo(:), hi(:), ng
     real (kind = dp_t), intent(inout) ::   viscosity(lo(1)-ng:,lo(2)-ng:)  
-    real (kind = dp_t), intent(in   ) :: strain_rate(lo(1)   :,lo(2)   :)  
+    real (kind = dp_t), intent(in   ) ::         phi(lo(1)   :,lo(2)   :)  
     real (kind = dp_t), intent(in   ) :: dx(:)
 
     integer :: i, j
@@ -80,53 +80,31 @@ contains
 
     do j = lo(2), hi(2)
        do i = lo(1), hi(1)
-          frac = strain_rate(i,j) / papa_reg
-          if (frac < 1e-9) then
-             ! Taylor expand exp() to avoid dividing by small number
-             viscosity(i,j) = visc_coef + yield_stress / papa_reg * &
-                (ONE - HALF * frac + SIXTH * frac**2 - SIXTH * FOURTH * frac**3)
-          else 
-             viscosity(i,j) = visc_coef + yield_stress / papa_reg * &
-                (ONE - exp(-frac)) / frac
-          endif
+       ! TODO: make level-set dependent
+          viscosity(i,j) = viscosity(i,j)
        enddo
     enddo
 
   end subroutine update_viscosity_2d
 
   ! TODO: 
-  subroutine update_viscosity_3d(visc, u, lo, hi, ng_v, ng_u, dx)
+  subroutine update_viscosity_3d(visc, phi, lo, hi, ng, dx)
 
     use bl_constants_module
-    use probin_module, only: visc_coef, yield_stress, papa_reg
+    use probin_module, only: visc_coef
 
-    integer           , intent(in   ) :: lo(:), hi(:), ng_v, ng_u
-    real (kind = dp_t), intent(inout) ::    visc(lo(1)-ng_v:,lo(2)-ng_v:,lo(3)-ng_v:,:)  
-    real (kind = dp_t), intent(in   ) ::       u(lo(1)-ng_u:,lo(2)-ng_u:,lo(3)-ng_u:,:)  
+    integer           , intent(in   ) :: lo(:), hi(:), ng
+    real (kind = dp_t), intent(inout) ::    visc(lo(1)-ng:,lo(2)-ng:,lo(3)-ng:,:)  
+    real (kind = dp_t), intent(in   ) ::     phi(lo(1)   :,lo(2)   :,lo(3)   :,:)  
     real (kind = dp_t), intent(in   ) :: dx(:)
 
     integer :: i, j, k
-    real (kind = dp_t) ux, uy, uz, vx, vy, vz, wx, wy, wz, dotgamma, frac
 
     do k = lo(3), hi(3)
        do j = lo(2), hi(2)
           do i = lo(1), hi(1)
-             ux = (u(i+1,j,k,1) - u(i-1,j,k,1)) / (TWO * dx(1))
-             uy = (u(i,j+1,k,1) - u(i,j-1,k,1)) / (TWO * dx(2))
-             uz = (u(i,j,k+1,1) - u(i,j,k-1,1)) / (TWO * dx(3))
-             vx = (u(i+1,j,k,2) - u(i-1,j,k,2)) / (TWO * dx(1))
-             vy = (u(i,j+1,k,2) - u(i,j-1,k,2)) / (TWO * dx(2))
-             vz = (u(i,j,k+1,2) - u(i,j,k-1,2)) / (TWO * dx(3))
-             wx = (u(i+1,j,k,3) - u(i-1,j,k,3)) / (TWO * dx(1))
-             wy = (u(i,j+1,k,3) - u(i,j-1,k,3)) / (TWO * dx(2))
-             wz = (u(i,j,k+1,3) - u(i,j,k-1,3)) / (TWO * dx(3))
-             dotgamma = sqrt(TWO * (ux**2 + vy**2 + wz**2) + (uy + vx)**2 + (vz + wy)**2 + (wx + uz)**2)
-             frac = dotgamma / papa_reg
-             if (frac < 1e-5) then
-                visc(i,j,k,1) = visc_coef + yield_stress / papa_reg * (ONE - HALF * frac + SIXTH * frac**2)
-             else 
-                visc(i,j,k,1) = visc_coef + yield_stress / dotgamma * (ONE - exp(-frac))
-             endif
+        ! TODO: make level-set dependent
+            viscosity(i,j) = viscosity(i,j)
           enddo
        enddo
     enddo
